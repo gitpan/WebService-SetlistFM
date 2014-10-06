df42793b7799f64623f038bb05d98d58a7731aef
Daisuke Kubota (HONDALLICA) <hondallica@gmail.com>
2014-10-06 09:01:51 +0000

    Import of HONDALLIC/WebService-SetlistFM-0.02 from CPAN.
    gitpan-cpan-distribution: WebService-SetlistFM
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         HONDALLIC/WebService-SetlistFM-0.02.tar.gz
    gitpan-cpan-author:       HONDALLIC
    gitpan-cpan-maturity:     released

--- lib/WebService/SetlistFM.pm
+++ lib/WebService/SetlistFM.pm
@@ -8,7 +8,7 @@ use URI::QueryParam;
 use Carp;
 use Moo;
 use namespace::clean;
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 
 $Net::DNS::Lite::CACHE = Cache::LRU->new( size => 512 );
@@ -158,7 +158,7 @@ __END__
 
 =head1 NAME
 
-WebService::SetlistFM - A simple and fast interface to the www.setlist.fm API
+WebService::SetlistFM - A simple and fast interface to the L<http://www.setlist.fm> API
 
 =head1 SYNOPSIS
 
@@ -173,10 +173,11 @@ WebService::SetlistFM - A simple and fast interface to the www.setlist.fm API
 
 =head1 DESCRIPTION
 
-The module provides a simple interface to the www.setlist.fm API.
+The module provides a simple interface to the L<http://www.setlist.fm> API.
 
 =head1 METHODS
-These methods usage: http://api.setlist.fm/docs/ 
+
+These methods usage: L<http://api.setlist.fm/docs/>
 
 =head3 artist
 
@@ -257,7 +258,7 @@ These methods usage: http://api.setlist.fm/docs/
 
 =head1 SEE ALSO
 
-http://api.setlist.fm/docs/
+L<http://api.setlist.fm/docs/>
 
 =head1 LICENSE
 
